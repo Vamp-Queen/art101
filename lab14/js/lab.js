10eb72cfeaebdab593ae9a80a6f5638968362fa3
Hailey <winterrose1895@gmail.com>
2025-05-30 00:26:15 -0700

    lab14 upload

---
+++ lab14/js/lab.js
@@ -0,0 +1,8 @@
+/* 
+ *  lab.js - This function takes a user-inputted number and loops through each number, outputting words at certain numbers. 
+ *  Requirements: jQuery must be loaded for this script to work. 
+
+ *  Author: Hailey Phipps
+ *  Created: May 26, 2025
+ */ 
+
